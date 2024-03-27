5b7d3886884d9284aee0ffe953070b16e415c773
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-03-28 04:03:41 +0700

    working with austria proxy

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import datetime
 import re
 import time
+from random import choice
+from typing import List
 from urllib.parse import urljoin
 import httpx
 from httpx import Client
 from selectolax.parser import HTMLParser
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import sqlite3
 import requests
 import pandas as pd
@@ -13,6 +15,11 @@ import pandas as pd
 @dataclass
 class PlayWithAmazon:
     base_url: str = 'https://www.amazon.de'
+    proxies: List = field(default_factory=lambda: ['xfmmdfbe:kbf25s6qb4ei@138.128.145.137:6056',
+                          'xfmmdfbe:kbf25s6qb4ei@64.137.103.27:6615',
+                          'xfmmdfbe:kbf25s6qb4ei@64.137.103.229:6817',
+                          'xfmmdfbe:kbf25s6qb4ei@138.128.145.132:6051',
+                          'xfmmdfbe:kbf25s6qb4ei@64.137.103.123:6711'])
 
     def fetch(self, headers, url):
         conn = sqlite3.connect('amazon.db')
@@ -25,8 +32,13 @@ class PlayWithAmazon:
             ) 
             """
         )
+        proxy = choice(self.proxies)
+        proxy = {
+            "http": f"http://{proxy}/",
+            "https": f"http://{proxy}/"
+        }
 
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, proxies=proxy)
         time.sleep(1)
         if response.status_code != 200:
             response.raise_for_status()
@@ -64,8 +76,14 @@ class PlayWithAmazon:
         for data in datas:
             tree = HTMLParser(data[1])
             timestamp = datetime.datetime.now()
-            asin = tree.css_first('div#productDetails_db_sections > div > table > tbody > tr >td').text(strip=True)
-            seller_name = tree.css_first('div[data-csa-c-slot-id="odf-feature-text-desktop-merchant-info"]').text(strip=True)
+            try:
+                asin = tree.css_first('div#productDetails_db_sections > div > table > tbody > tr >td').text(strip=True)
+            except:
+                asin = ''
+            try:
+                seller_name = tree.css_first('div[data-csa-c-slot-id="odf-feature-text-desktop-merchant-info"]').text(strip=True)
+            except:
+                seller_name = ''
             try:
                 pattern = r'seller=([A-Z0-9]+)'
                 input_string = tree.css_first('div[data-csa-c-slot-id="odf-feature-text-desktop-merchant-info"] > div > span > a').attributes.get('href')
@@ -80,7 +98,10 @@ class PlayWithAmazon:
                 price = tree.css_first('span.priceToPay').text(strip=True)
             except:
                 price_element = tree.css_first('div#apex_desktop')
-                price = price_element.css_first('span.a-price.a-text-price.a-size-medium.apexPriceToPay > span:nth-of-type(1)').text(strip=True)
+                if price_element.css_first('span.a-price.a-text-price.a-size-medium.apexPriceToPay > span:nth-of-type(1)'):
+                    price = price_element.css_first('span.a-price.a-text-price.a-size-medium.apexPriceToPay > span:nth-of-type(1)').text(strip=True)
+                else:
+                    price = ''
             try:
                 shipping_cost = tree.css_first('span[data-csa-c-type="element"]').attributes.get('data-csa-c-delivery-price')
                 delivery_date = tree.css_first('span[data-csa-c-type="element"]').attributes.get('data-csa-c-delivery-time')
@@ -120,20 +141,25 @@ class PlayWithAmazon:
         }
 
         asins = ['B000O6XSFO', 'B0C9Q68TDG', 'B07TL8T5T8', 'B09VPQ5ZTR', 'B0BL3XWYJ5', 'B0035F1DCG', 'B09VC7XWD9', 'B0761VD7L4', 'B09KPWTYNQ', 'B09VP7BZC8']
-        self.truncate_db()
+        # self.truncate_db()
         # self.drop_db()
         #
+
         for asin in asins:
-            # client = Client(headers=custom_headers, follow_redirects=True, http2=True)
-            endpoint = f'/dp/{asin}?th=1'
-            url = urljoin(self.base_url, endpoint)
-            self.fetch(headers=custom_headers, url=url)
-        #
+            try:
+                # client = Client(headers=custom_headers, follow_redirects=True, http2=True)
+                endpoint = f'/dp/{asin}?th=1'
+                url = urljoin(self.base_url, endpoint)
+                self.fetch(headers=custom_headers, url=url)
+            except Exception as e:
+                print(e)
+                continue
+
         # self.cek_db()
 
         product_datas = list(self.get_data())
         df = pd.DataFrame.from_records(columns=['asin', 'timestamp', 'seller_name', 'seller_token', 'price', 'shipping_cost', 'delivery_date'], data=product_datas)
-        df.to_csv('10_amazon_sample_result_rev1.csv', index=False)
+        df.to_csv('10_amazon_sample_result.csv', index=False)
 
         conn = sqlite3.connect("amazon.db")
         curr = conn.cursor()
